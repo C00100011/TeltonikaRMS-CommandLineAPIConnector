5cc7716b2850ff83510ae672827b51ea3ad8a08c
Gideon <30596579+DarkIodine@users.noreply.github.com>
2021-02-17 08:22:53 +0100

    Initial commit

---
+++ TeltonikaRemote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeltonikaRemote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
